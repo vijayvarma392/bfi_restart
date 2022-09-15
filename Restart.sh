71b0e570004f90ce12368ece0a424b8b8d883acd
Vijay Varma <vijay.varma392@gmail.com>
2022-09-15 05:43:04 -0400

    Update Restart

--- Restart.sh
+++ Restart.sh
@@ -25,5 +25,5 @@ LOGFILE=".bfi_restart/"$ProjectNumberOrName".log"
 
 # Run RestartProject and send email with summary
 ( RestartProject -r $ProjectNumberOrName) \
-    && (tail -n 6 $LOGFILE |  mail -s "RestartProject -r $ProjectNumberOrName ended after $(secs_to_human $(($(date +%s) - ${start}))), on machine=$ThisMachine id=$JobID" $EMAIL && echo Mail sent) \
-    || (tail -n 6 $LOGFILE |  mail -s "RestartProject -r $ProjectNumberOrName Failed after $(secs_to_human $(($(date +%s) - ${start}))), on machine=$ThisMachine id=$JobID" $EMAIL && echo Mail sent && exit $?)
+    && (tail -n 6 $LOGFILE |  mail -s "RestartProject -r $ProjectNumberOrName ended after $(secs_to_human $(($(date +%s) - ${start}))), on machine=$ThisMachine id=$JobID" $EMAIL && echo Mail sent to $EMAIL) \
+    || (tail -n 6 $LOGFILE |  mail -s "RestartProject -r $ProjectNumberOrName Failed after $(secs_to_human $(($(date +%s) - ${start}))), on machine=$ThisMachine id=$JobID" $EMAIL && echo Mail sent to $EMAIL && exit $?)
